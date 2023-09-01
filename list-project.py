8875ae8305973bf5b1899af83f6695cd334bebc8
Ester Molinari <64893048+burraco135@users.noreply.github.com>
2023-09-01 12:46:00 +0200

    Update list-project.py

--- list-project.py
+++ list-project.py
@@ -1,39 +1,45 @@
 import requests
-import json
 
-def main(args):
-    organization = args.get('organization')
-    github_token = args.get('GITHUB_TOKEN')
-        
-    if github_token is None:
-        return {'error': 'GitHub token not set'}
+def check_errors(response):
+    if response.status_code == 200:
+        return None
+    return {"body": f"error: Github API Error: {response.reason}"}
 
+def list_projects(organization, token):
+    if not organization or not token:
+        return {"body": "error: Missing organization or GitHub token"}
+    
     headers = {
-        'Authorization': f'Bearer {github_token}',
+        'Authorization': f'Bearer {token}',
         'Accept': 'application/vnd.github.v3+json'
     }
 
-    # Se non viene trovato il nome dell'organizzazione, restituisce un errore
-    if organization is None:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({'error': 'Organization name not exists'})
-        }
-    
     url = f'https://api.github.com/orgs/{organization}/repos'
-    
+
     response = requests.get(url, headers=headers)
+
+    return response
+
+def main(args):
+    organization = args.get('organization')
+    token = args.get('githubtoken')
+    
+    response = list_projects(organization, token)
+    error = check_errors(response)
+
+    if error:
+        return {
+            'statusCode': response.status_code,
+            'body': error
+        }
 
     repositories = response.json()
     
     projects = {}
 
-    if response.status_code == 200:
-        for repo in repositories:
-            repo_name = repo["name"]
-            repo_description = repo["description"]
-            projects[repo_name] = repo_description
+    for repo in repositories:
+        repo_name = repo["name"]
+        repo_description = repo["description"]
+        projects[repo_name] = repo_description
 
-        return {'repos': projects}
-    else:
-        raise Exception(f"Error: {response.status_code}, {response.text}")
+    return {"body": projects}
